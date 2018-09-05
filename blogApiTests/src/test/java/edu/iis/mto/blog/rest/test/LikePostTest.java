07ec8ff19fa95b6a1072d5ff075360370cbca06b
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-09-05 03:58:28 +0200

    Added test likingPostTwiceDoesNotChangeLikesCount

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -38,6 +38,24 @@ package edu.iis.mto.blog.rest.test;
          RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                  .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
                  .post(CONFIRMED_USER_OWNER_OF_POST);
+     }
      
+     @Test
+     public void likingPostTwiceDoesNotChangeLikesCount() {
+         JSONObject jsonObj = new JSONObject();
+
+         String urlGetFirstPost = "/blog/user/1/post";
+         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                 .body(jsonObj.toString()).expect().log().all().body(containsString("\"likesCount\":1")).when()
+                 .get(urlGetFirstPost);
+
+         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                 .post(CONFIRMED_USER_NOT_OWNER_OF_POST);
+
+         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                 .body(jsonObj.toString()).expect().log().all().body(containsString("\"likesCount\":1")).when()
+                 .get(urlGetFirstPost);
+     }
      
  }
