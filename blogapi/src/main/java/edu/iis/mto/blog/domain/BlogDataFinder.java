14d5752f3b7e0dd2df562c1742df3859b6d784ff
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-09-05 03:51:33 +0200

    Added test searchingPostShouldReturnAppriopriateLikeCount

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.User;
 import edu.iis.mto.blog.dto.PostData;
@@ -38,12 +40,20 @@ public class BlogDataFinder extends DomainService implements DataFinder {
 
     @Override
     public PostData getPost(Long userId) {
+        User user = userRepository.findOne(userId);
+        if(user.getAccountStatus().equals(AccountStatus.REMOVED)) {
+            throw new DomainError("could not get removed user posts");
+        }
         BlogPost blogPost = blogPostRepository.findOne(userId);
         return mapper.mapToDto(blogPost);
     }
 
     @Override
     public List<PostData> getUserPosts(Long userId) {
+    	User user = userRepository.findOne(userId);
+    	if(user.getAccountStatus().equals(AccountStatus.REMOVED)){
+            throw new DomainError("could not get removed user posts");
+        }
         User user = userRepository.findOne(userId);
         List<BlogPost> posts = blogPostRepository.findByUser(user);
         return posts.stream().map(post -> mapper.mapToDto(post)).collect(Collectors.toList());
